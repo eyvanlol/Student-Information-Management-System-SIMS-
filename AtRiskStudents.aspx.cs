fa4d6d1e74c45e0e3c12b1853c12f77b09fad729
soonyx <soonyixian123@gmail.com>
2026-06-23 16:20:28 +0800

    New Risk Student

--- AtRiskStudents.aspx.cs
+++ AtRiskStudents.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Data;
 using System.Data.SqlClient;
+using System.Web.UI.WebControls;
 
 namespace StudentManagementSystem
 {
@@ -22,6 +23,18 @@ namespace StudentManagementSystem
             LoadAtRiskStudents(); // run every load so system always checks risk
         }
 
+        protected void gvLowAttendance_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "SendWarning")
+            {
+                int rowIndex = Convert.ToInt32(e.CommandArgument);
+                int studentID = Convert.ToInt32(gvLowAttendance.DataKeys[rowIndex].Value);
+
+                // Your logic here to send warning to student
+                lblSystemMessage.Text = "Warning sent to student.";
+                pnlMessage.Visible = true;
+            }
+        }
         private void LoadAtRiskStudents()
         {
             int lecturerID = Convert.ToInt32(Session["UserID"]);
